c5464e2bbdfcb8f3ca21c35242ba646bbe308db2
inkevap <inkev.ap@gmail.com>
2022-08-12 23:51:42 -0600

    introduccion

---
+++ 00-Introduccion/mi_primer_proyecto/index.js
@@ -0,0 +1,10 @@
+//Este es el mensaje de bienvenida
+console.log("Esta es la puerta de entrada al proyecto");
+
+/*
+    Este es el mensaje que se envia para despedir
+    En esta parte realizamos los chequeos necesarios
+    por ultimo vamos a la ultima linea de saludo
+*/
+
+console.log("adios");
