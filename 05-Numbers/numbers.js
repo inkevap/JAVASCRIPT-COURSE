2b0046665e08260178f6dfcb40b98df59c4e627d
inkevap <inkev.ap@gmail.com>
2022-08-13 22:06:43 -0600

    tarea

--- 05-Numbers/numbers.js
+++ 05-Numbers/numbers.js
@@ -3,7 +3,7 @@ let altura_mts = 1.73;
 let peso_kg = 59.5;
 let altura_mts_round = Math.ceil(altura_mts);
 let peso_kg_round = Math.floor(peso_kg);
-let valor_maximo = Number.MAX_VALUE + 1;
+let valor_maximo = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
 
 console.log(altura);
 console.log(altura_mts);
