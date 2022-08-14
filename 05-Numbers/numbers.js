389a78cd451f6c3e0f808a198e31f0c306aaced0
inkevap <inkev.ap@gmail.com>
2022-08-13 22:05:04 -0600

    Tarea

---
+++ 05-Numbers/numbers.js
@@ -0,0 +1,13 @@
+let altura = 173;
+let altura_mts = 1.73;
+let peso_kg = 59.5;
+let altura_mts_round = Math.ceil(altura_mts);
+let peso_kg_round = Math.floor(peso_kg);
+let valor_maximo = Number.MAX_VALUE + 1;
+
+console.log(altura);
+console.log(altura_mts);
+console.log(peso_kg);
+console.log(altura_mts_round);
+console.log(peso_kg_round);
+console.log(valor_maximo);
