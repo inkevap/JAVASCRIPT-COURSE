7097976fbd640f8ed7cdb6309a027ac4080918f4
inkevap <inkev.ap@gmail.com>
2022-08-14 12:24:30 -0600

    Tarea

---
+++ 07-Tipos_de_objetos/conjuntos.js
@@ -0,0 +1,11 @@
+let familia = ["Kevin", "Calvin","Fredy","Rosy"];
+console.log(familia);
+
+setFamilia = new Set(familia);
+console.log(setFamilia);
+
+setFamilia.add("Kevin");
+console.log(setFamilia);
+
+setFamilia.add("Javascript");
+console.log(setFamilia);
